764cdfc549dd7629913de1c9677c10a82325ffb3
DiptoPramanik <diptopramanikprogramming05@gmail.com>
2025-01-18 04:15:32 +0600

    update again

--- binary-searching/problems/Light OJ/Expanding Rods.cpp
+++ binary-searching/problems/Light OJ/Expanding Rods.cpp
@@ -8,21 +8,23 @@ int main()
     int t; cin>>t;
     for(int tc=1;tc<=t;tc++)
     {
-        dl x,y,c; cin>>x>>y>>c;
-        dl l =0,r=min(x,y);
+        dl L,n,c; cin>>L>>n>>c;
         dl eps = 1e-7;
+        dl l1 = ((1+(n*c))*L);
+        dl l=0,r=1e18;
         while(r-l>eps)
         {
-            dl mid = (l+r)/2;
-            dl d = mid;
-            dl h1 = sqrt((x*x)-(d*d));
-            dl h2 = sqrt((y*y)-(d*d));
-            dl h = ((h1*h2)/(h1+h2));
-            if(h<c) r = mid;
+            dl mid =(l+r)/2;
+            dl R = mid;
+            dl s= 2*R*asin(L/(2*R));
+            if(s<l1) r=mid;
             else l = mid;
         }
+        dl R = l;
+        dl d = sqrt((R*R)-(L/2)*(L/2));
+        dl h = R - d;
         fraction();
-        cout<<"Case "<<tc<<": "<<l<<'\n';
+        cout<<"Case "<<tc<<": "<<h<<'\n';
     }
     return 0;
 }
